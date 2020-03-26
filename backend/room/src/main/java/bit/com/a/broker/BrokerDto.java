766b684665ae9c2e09d405d961bd7d2d360d7c5d
LEE ON YOU <dhsdb1541@naver.com>
2020-03-26 16:20:07 +0900

    fix

--- backend/room/src/main/java/bit/com/a/broker/BrokerDto.java
+++ backend/room/src/main/java/bit/com/a/broker/BrokerDto.java
@@ -10,10 +10,12 @@ import javax.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data // getter setter 자동생성
 @AllArgsConstructor // 모든 생성자(전체 포함, 일부생성자는 만들어줘야함)
 @NoArgsConstructor // 기본 생성자 UserDto(){}
+@ToString
 // @Entity
 public class BrokerDto {
 
