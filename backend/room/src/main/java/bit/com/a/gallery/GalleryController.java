4398e935a09e317e405a59439e0f0fc86f295aee
LEE ON YOU <dhsdb1541@naver.com>
2020-04-02 09:14:47 +0900

    fix

--- backend/room/src/main/java/bit/com/a/gallery/GalleryController.java
+++ backend/room/src/main/java/bit/com/a/gallery/GalleryController.java
@@ -1,17 +1,13 @@
 package bit.com.a.gallery;
 
 import java.io.IOException;
-<<<<<<< HEAD
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.web.bind.annotation.GetMapping;
-=======
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.ConfigurationProperties;
->>>>>>> 3d021aaa15637090eaa745c486f815a17cc7f7b5
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,23 +33,15 @@ public class GalleryController {
   @PostMapping("/upload")
   public String execWrite(GalleryDto galleryDto, MultipartFile file) throws IOException {
     String imgPath = s3Service.upload(file);
-<<<<<<< HEAD
     galleryDto.setFilePath("https://" + "dixo0q5vi6g16.cloudfront.net" + "/" + imgPath);
-=======
-    galleryDto.setFilePath(imgPath);
->>>>>>> 3d021aaa15637090eaa745c486f815a17cc7f7b5
-
     return galleryService.upload(galleryDto) > 0 ? "OK" : "FAIL";
   }
 
-<<<<<<< HEAD
   @GetMapping("/getImageList")
   public List<GalleryDto> getImageList() {
     return galleryService.getImageList();
   }
 
-=======
->>>>>>> 3d021aaa15637090eaa745c486f815a17cc7f7b5
   // @PostMapping("/upload")
   // public String upload(MultipartFile file) throws IOException {
   // GalleryDto galleryDto = new GalleryDto();
